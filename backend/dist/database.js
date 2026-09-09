c23788ca5090305b929709467351bfe7a472fef4
Vibhore Mathur <onlyvibhore@gmail.com>
2026-09-09 13:05:20 +0530

    remove dist

--- backend/dist/database.js
+++
@@ -1,6 +0,0 @@
-import { Sequelize } from "sequelize";
-import { config } from "./config.js";
-export const sequelize = new Sequelize(config.databaseUrl, {
-    dialect: "postgres",
-    logging: false,
-});
